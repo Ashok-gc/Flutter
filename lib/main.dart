e27b6a8326f8cb443dc4a90c5291ca17dabf4b4e
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2022-12-11 16:04:44 +0545

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,54 @@
 
+// import 'package:flutter/material.dart';
+// import 'package:my_firstapp/screens/cardscreen.dart';
+// void main() {
+//   runApp(
+//     const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home:CardScreen(),
+//     ),
+//   );
+// }
+
+
 import 'package:flutter/material.dart';
-import 'package:my_firstapp/screens/cardscreen.dart';
+import 'package:my_firstapp/SnackBar.dart';
+import 'package:my_firstapp/arithmetic.dart';
+import 'package:my_firstapp/arithmetic_output.dart';
+import 'package:my_firstapp/assignment.dart';
+import 'package:my_firstapp/buttons.dart';
+import 'package:my_firstapp/columnscreen.dart';
+import 'package:my_firstapp/container.dart';
+import 'package:my_firstapp/flexible.dart';
+import 'package:my_firstapp/imagescreen.dart';
+import 'package:my_firstapp/radiobutton.dart';
+import 'package:my_firstapp/rowcolumn.dart';
+import 'package:my_firstapp/screens/student_screen.dart';
+
 void main() {
   runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:CardScreen(),
-    ),
+    MaterialApp(
+      initialRoute: '/studentScreen',
+      routes: {
+        '/':(context) => const StudentDetailsScreen(),
+        '/snackBar':(context) => const SnackBarScreen(),
+        '/arithmeticScreen':(context) => const ArithmeticScreen(),
+        '/arithoutput':(context) => const arithoutput(),
+        '/studenScreen':(context) => const StudenScreen(),
+        '/buttons':(context) => const buttons(),
+        '/columnScreen':(context) => const ColumnScreen(),
+        '/container':(context) => const ContainerScreen(),
+        '/image':(context) => const ImageScreen(),
+        '/flexibleScreen':(context) => const FlexibleScreen(),
+        '/radioButton':(context) => const radiobutton(),
+        '/rowColumn':(context) => const ImageScreen(),
+        '/rowColumnScreen':(context) => const RowColumnScreen(),
+        '/studentScreen':(context) => const StudentDetailsScreen(),
+        // '/displayStudent':(context) => const DisplayStudentDetailsScreen(),
+
+
+      },
+    )
   );
+  
 }
--- lib/screens/displayStudent.dart
+++ lib/screens/displayStudent.dart
@@ -1,50 +1,50 @@
-import 'dart:js';
+// import 'dart:js';
 
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/src/widgets/container.dart';
+// import 'package:flutter/src/widgets/framework.dart';
 
-import '../model/student.dart';
+// import '../model/student.dart';
 
-class DisplayStudentDetailsScreen extends StatefulWidget {
-  // const DisplayStudentDetailsScreen({super.key});
-  List<Student> lstStudent =[];
+// class DisplayStudentDetailsScreen extends StatefulWidget {
+//   // const DisplayStudentDetailsScreen({super.key});
+//   List<Student> lstStudent =[];
 
-  @override
-  void didChangeDependencies() {
-    lstStudent=ModalRoute.of(context)!.settings.arguments as List<Student>;
-    super.didChangeDependencies();
+//   @override
+//   void didChangeDependencies() {
+//     lstStudent=ModalRoute.of(context)!.settings.arguments as List<Student>;
+//     super.didChangeDependencies();
     
-  }
-}
+//   }
+// }
 
-class _DisplayStudentDetailsScreenState extends State<DisplayStudentDetailsScreen> {
-  @override
-  Widget build(BuildContext context) {
-      var lstStudent;
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Student Details'),
-          elevation: 0,
-        ),
-        body: lstStudent.isNotEmpty
-            ? ListView.builder(
-              itemCount: lstStudent.length,
-              itemBuilder: (BuildContext context, int index){
-                return ListTile(
-                  title: Text(lstStudent[index].fname! +
-                  ' ' +
-                  lstStudent[index].lname!),
-                subtitle: Text(lstStudent[index].address!),
-                trailing: Wrap(
-                  children: [
-                    IconButton(onPressed: (){}, icon: const)
-                  ],
-                ),
-                )
-              },
+// class _DisplayStudentDetailsScreenState extends State<DisplayStudentDetailsScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//       var lstStudent;
+//       return Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Student Details'),
+//           elevation: 0,
+//         ),
+//         body: lstStudent.isNotEmpty
+//             ? ListView.builder(
+//               itemCount: lstStudent.length,
+//               itemBuilder: (BuildContext context, int index){
+//                 return ListTile(
+//                   title: Text(lstStudent[index].fname! +
+//                   ' ' +
+//                   lstStudent[index].lname!),
+//                 subtitle: Text(lstStudent[index].address!),
+//                 trailing: Wrap(
+//                   children: [
+//                     IconButton(onPressed: (){}, icon: const)
+//                   ],
+//                 ),
+//                 )
+//               },
 
-            )
-      );
-  }
-}
+//             )
+//       );
+//   }
+// }
